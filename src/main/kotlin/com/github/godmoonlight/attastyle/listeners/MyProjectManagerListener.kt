c96ebce707bd8533165186deab5d6b76231130c0
GitHub Action <action@github.com>
2020-09-06 07:59:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/godmoonlight/attastyle/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/godmoonlight/attastyle/listeners/MyProjectManagerListener.kt
@@ -1,8 +1,8 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.godmoonlight.attastyle.listeners
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.godmoonlight.attastyle.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/godmoonlight/attastyle/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/godmoonlight/attastyle/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.godmoonlight.attastyle.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.godmoonlight.attastyle.MyBundle
 
 class MyApplicationService {
 
